e7b51b8fb615d08ff8f68d21c03e711392e61184
Unnati-Kuradkar <unnatikuradkar@gmail.com>
2026-07-13 21:54:54 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="BharatAI",
+    layout="wide"
+)
+
+st.title("🇮🇳 BharatAI")
+st.subheader("AI Government Opportunities Assistant")
+
+st.success("Streamlit is working successfully!")
